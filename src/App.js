17c69cb28445c615b68d2f7ed3bab992eb50e7cd
Timur Mishiev <timurmishiev@timurs-mbp.myfiosgateway.com>
2022-02-03 20:51:16 -0500

    add logic for changing pages

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
 import { Posts } from "./conponents/Posts";
+import { Pagination } from "./conponents/Pagination";
 
 const App = () => {
 	const [posts, setPosts] = useState([]);
 	const [loading, setLoading] = useState(false);
 	const [currentPage, setCurrentPage] = useState(1);
-	const [postsPerPage, setPostsPerPage] = useState(10);
+	const [postsPerPage] = useState(10);
 
 	useEffect(() => {
 		const fetchPosts = async () => {
@@ -26,10 +27,20 @@ const App = () => {
 	const indexOfFirstPost = indexOfLastPost - postsPerPage;
 	const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
 
+	//Change page
+	const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
 	return (
 		<div className="container mt-5">
-			<h1 className="text-primary mb-3">Simple Pagination with get request</h1>
+			<h1 className="text-primary mb-3">
+				Simple Pagination with fatched Posts
+			</h1>
 			<Posts posts={currentPosts} loading={loading} />
+			<Pagination
+				postsPerPage={postsPerPage}
+				totalPosts={posts.length}
+				paginate={paginate}
+			/>
 		</div>
 	);
 };
