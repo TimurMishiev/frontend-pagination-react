3c1ae9e8161d336c9bf8e830a68c0f5dd04e5376
Timur Mishiev <timurmishiev@timurs-mbp.myfiosgateway.com>
2022-02-03 20:53:37 -0500

    fix a typo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
-import { Posts } from "./conponents/Posts";
-import { Pagination } from "./conponents/Pagination";
+import { Posts } from "./components/Posts";
+import { Pagination } from "./components/Pagination";
 
 const App = () => {
 	const [posts, setPosts] = useState([]);
